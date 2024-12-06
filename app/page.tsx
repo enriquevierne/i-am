7881d3fa048b304cf7e7855ad794ec40085216d4
Enrique Vierne <110237705+enriquevierne@users.noreply.github.com>
2024-12-06 17:18:01 -0300

    firts commit and initial config

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-8">
+            <span>Hey, I am</span>
+            <h1>Enrique Vierne</h1>
+            <p>Full Stack Web Developer</p>
+        </div>
+    );
+}
