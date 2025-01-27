ed8ae9328b0cd547765c88976430ac104cfd2cce
Enrique Vierne <110237705+enriquevierne@users.noreply.github.com>
2025-01-27 16:53:41 -0300

    feat: add some components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,39 @@
 import { Badge } from "@/components/ui/badge";
 import { Github, Linkedin, MessageCircle } from "lucide-react";
 import Link from "next/link";
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
+interface Project {
+    title: string;
+    desc: string;
+    deploy: string;
+    repo: string;
+}
 
 export default function Home() {
+    const projectList: Project[] = [
+        {
+            title: "Siiiiiiiiiiiiuuuu",
+            desc: " How much does CR7 earn from Al Nassr?",
+            deploy: "https://cr7.enriquevierne.dev/",
+            repo: "https://github.com/enriquevierne/Siiiiiiiiiiiiuuuu",
+        },
+        {
+            title: "I-am",
+            desc: "My personal portfolio",
+            deploy: "https://i-am.enriquevierne.dev/",
+            repo: "https://github.com/enriquevierne/i-am",
+        },
+    ];
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-4">
             <div className="size-28 animate-wiggle ">
@@ -56,6 +87,47 @@ export default function Home() {
                     aria-label="Download curriculum in English">
                     en-US
                 </a>
+            </div>
+            <div>
+                <p className="text-xl font-bold py-6 w-full text-center">
+                    My public projects
+                </p>
+                <ul className="w-full grid grid-cols-2 gap-6">
+                    {projectList.map((project) => {
+                        return (
+                            <Card
+                                key={project.deploy}
+                                className="w-[350px] col-span-2 sm:col-span-1">
+                                <CardHeader>
+                                    <CardTitle>{project.title}</CardTitle>
+                                    <CardDescription>
+                                        {project.desc}
+                                    </CardDescription>
+                                </CardHeader>
+                                <CardFooter className="flex justify-between gap-2">
+                                    <Button className="w-full">
+                                        <Link
+                                            href={project.deploy}
+                                            target="_blank">
+                                            {" "}
+                                            visit
+                                        </Link>
+                                    </Button>
+                                    <Button
+                                        className="w-full"
+                                        variant={"ghost"}>
+                                        <Link
+                                            href={project.repo}
+                                            target="_blank">
+                                            {" "}
+                                            repo
+                                        </Link>
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        );
+                    })}
+                </ul>
             </div>
         </div>
     );
